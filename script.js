1b5af12f69b57dbd778be3f0cf82f56a6543beb4
Katelynn Cowan <kcowan@twu.edu>
2020-11-07 17:31:34 -0600

    added date and time variables

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-script.js
+var currentTime = moment().format('hh:mm:ss');
+    $("#time-display").text(currentTime);
+var currentDate = moment().format('MMM Do, YYYY');
+    $("#date-display").text(currentDate);
+
